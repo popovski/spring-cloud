35ec9ddfb935f5f99e158870781d8ce2302d6b16
Nikola Popovski <popovski.nikola@gmail.com>
2020-04-12 12:57:07 +0200

    Code refactor

--- service-app/src/main/java/mk/factory/code/api/TestController.java
+++ service-app/src/main/java/mk/factory/code/api/TestController.java
@@ -1,19 +1,37 @@
 package mk.factory.code.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import mk.factory.code.config.ConfClientAppConfiguration;
 
 @RestController
 @RequestMapping("/")
+@RefreshScope
 public class TestController {
 	@Value("${service.instance.name}")
 	private String instance;
 	
+	@Autowired
+    ConfClientAppConfiguration confClientAppConfiguration;
+	
+	@Value("${some.other.property}")
+	private String someOtherProperty;
+	
 	@GetMapping
 	public String message() {
-		return "Hello from " + instance;
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("confClientAppConfiguration Property: " + confClientAppConfiguration.getProperty());
+		sb.append("\n");
+		sb.append("Property @VALUE: " + someOtherProperty);
+		
+		return sb.toString();
 	}
 }
---
+++ service-app/src/main/java/mk/factory/code/config/ConfClientAppConfiguration.java
@@ -0,0 +1,18 @@
+package mk.factory.code.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="some")
+public class ConfClientAppConfiguration {
+	private String property;
+
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	} 
+}
